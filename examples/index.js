a3020d5232f7d3aebfe5dcb636069bc93b4c7083
Christian Broberg <cb@cb-pro.local>
2018-11-12 17:38:27 +0100

    Included example to test modules

--- examples/index.js
+++ examples/index.js
@@ -1,37 +1,14 @@
-var mqttalk = require('../mqttalk')
-// var module = require('mqttalk/lib/module')
+const authenticate = require('../lib/authenticate')
+const { encrypt, decrypt } = require('../lib/encryption')
+const verifyAPIVersion = require('../lib/verifyapiversion')
+const keys = require('../lib/loadenv')
 
-const jsonFromObject = mqttalk.jsonFromObject
-const jsonToObject = mqttalk.jsonToObject
-const execCmd = mqttalk.execCmd
-const timestamp = mqttalk.timestamp
-const encrypt = mqttalk.encrypt
-const decrypt = mqttalk.decrypt
-const createPayload = mqttalk.createPayload
-// const pub = mqttalk.pub
-// const sub = mqttalk.sub
+const { ENCRYPTION_KEY } = keys
 
 
-const payload = {
-	id: 1,
-	status: 'online',
-	timestamp: timestamp()
-}
+let enc = encrypt('Hello World')
+let token = encrypt(ENCRYPTION_KEY) 
+console.log(enc)
+console.log(decrypt(enc))
 
-let json = jsonFromObject(payload)
-let obj = jsonToObject(json)
-
-console.log(json)
-console.log(obj)
-
-let encrypted = encrypt('Dette bliver krypteret')
-
-console.log(encrypted)
-
-let decrypted = decrypt(encrypted)
-console.log(decrypted)
-
-console.log(createPayload('This is my message', 'senti/sensor/sentiwii/8020'))
-console.log(createPayload(encrypt('This is my message'), 'senti/sensor/sentiwii/8020'))
-
-console.log(execCmd('node', '-v'))
+console.log(authenticate(token))
