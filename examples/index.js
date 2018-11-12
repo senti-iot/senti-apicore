a42b1b147026c3388ef82503600a61e338d36981
Christian Broberg <cb@cb-pro.local>
2018-11-12 23:45:31 +0100

    Added api function to connect with authenticated to the senti API

--- examples/index.js
+++ examples/index.js
@@ -1,9 +1,8 @@
-const { authenticate } = require('../index')
-// const { encrypt, decrypt } = require('../index')
-const { encrypt } = require('../index')
-const { decrypt } = require('../index')
-// const verifyAPIVersion = require('../index')
 const loadenv = require('../lib/loadenv')
+// const { authenticate } = require('../index')
+const { api, authenticate, encrypt, decrypt } = require('../index')
+const verifyAPIVersion = require('../index').verifyapiversion
+// const { api } = require('../index')
 
 const { ENCRYPTION_KEY } = loadenv
 
@@ -11,4 +10,13 @@ let enc = encrypt('Hello World')
 let token = encrypt(ENCRYPTION_KEY) 
 console.log(enc)
 console.log(decrypt(enc))
-console.log(authenticate(token))
+console.log(authenticate(token)) // true
+console.log(verifyAPIVersion('v1')) // true
+
+const apiCall = async () => {
+	let response
+	response = await api.get('/template/v1')
+	console.log(response.data)
+}
+
+apiCall()
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 /*
- * Copyright (c) 2018-2018 apicore.js contributors.
+ * Copyright (c) 2018-2018 senti-apicore.js contributors.
  * Copyright (c) 2018-2018 Christian Broberg.
  *
  * See LICENSE for more information
@@ -10,25 +10,18 @@
 
 // Import modules
 
+const api = require('./lib/api')
 const authenticate = require('./lib/authenticate')
 const encrypt = require('./lib/encryption').encrypt
 const decrypt = require('./lib/encryption').decrypt
 const loadenv = require('./lib/loadenv')
 const verifyapiversion = require('./lib/verifyapiversion')
 
-/*
-const encrypt = require('./lib/encryption').encrypt
-const decrypt = require('./lib/encryption').decrypt
-*/
 
-// Expose mqttalk modules
+// Expose senti-apicore modules
+module.exports.api = api
 module.exports.authenticate = authenticate
 module.exports.encrypt = encrypt
 module.exports.decrypt = decrypt
 module.exports.loadenv = loadenv
 module.exports.verifyapiversion = verifyapiversion
-/*
-module.exports.encrypt = encrypt
-module.exports.decrypt = decrypt
-*/
-
