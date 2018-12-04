4137a38f299753e199833e4427f6eddea18109ec
Christian Broberg <cb@cb-pro.local>
2018-12-04 23:45:35 +0100

    Removed React specific encrypt/decrypt and reinstated api endpoint

--- index.js
+++ index.js
@@ -8,22 +8,18 @@
 
 // Import modules
 
-// const api = require('./lib/api')
+const api = require('./lib/api')
 const authenticate = require('./lib/authenticate')
 const encrypt = require('./lib/encryption').encrypt
-const encryptReact = require('./lib/encryption').encryptReact
 const decrypt = require('./lib/encryption').decrypt
-const decryptReact = require('./lib/encryption').decryptReact
 const loadenv = require('./lib/loadenv')
 const verifyapiversion = require('./lib/verifyapiversion')
 
 
 // Expose senti-apicore modules
-// module.exports.api = api
+module.exports.api = api
 module.exports.authenticate = authenticate
 module.exports.encrypt = encrypt
-module.exports.encryptReact = encryptReact
 module.exports.decrypt = decrypt
-module.exports.decryptReact = decryptReact
 module.exports.loadenv = loadenv
 module.exports.verifyapiversion = verifyapiversion
